fbfb465f8fbfbaa44a12fa938c6c5a54d72ecbc9
unknown <anusha@cronj.com>
2015-03-19 18:11:00 +0530

    removed unnecessary filter

--- client/src/app/controllers/mainCtrl.js
+++ client/src/app/controllers/mainCtrl.js
@@ -926,26 +926,6 @@ app.filter('smallize', function() {
     return input.substring(0,1).toLowerCase()+input.substring(1);
   }
 });
-app.filter('prettify', function () {
-   return function(json, scope) {
-        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-            var cls = 'number';
-            if (/^"/.test(match)) {
-                if (/:$/.test(match)) {
-                    cls = 'key';
-                } else {
-                    cls = 'string';
-                }
-            } else if (/true|false/.test(match)) {
-                cls = 'boolean';
-            } else if (/null/.test(match)) {
-                cls = 'null';
-            }
-            return '<span class="' + cls + '">' + match + '</span>';
-        });
-    }
-});
 
 //factory
 app.factory("XLSXReaderService", ['$q', '$rootScope',
