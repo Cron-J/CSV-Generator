f98f2315b6c18f8e6aa5d3e0e280cb991c74e99a
unknown <anusha@cronj.com>
2015-04-23 18:31:17 +0530

    added delimiter file

---
+++ server/model/delimeter.js
@@ -0,0 +1,27 @@
+/**
+ * @class Delimeters
+ * @classdesc delimeters contain field details
+ */
+var delimeter = {
+
+ 		includeHeader: {
+        type: Boolean
+    },
+
+    dateFormat : {
+        type: String
+    },
+
+    numberFormat : {
+        type: String
+    },
+
+    delimeterFormat : {
+        type: String
+    }
+
+};
+
+module.exports = {
+    Delimeter: delimeter
+}
