c499a7ed3a70d9459b7f8037ebd9655fdbc30ca0
gaurav14 <gaurav@cronj.com>
2015-05-11 16:25:21 +0530

    get json data to save

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,7 @@ exports.endpoints = [
     { method: 'POST', path: '/uploadedFileData', config: File.uploadFile },
     { method: 'GET', path: '/getAttributes', config: Common.getAll },
     { method: 'GET', path: '/getMappingList/{tenantId}', config: Mapping.getMappingList },
+    { method: 'GET', path: '/getTestMappingData/{tenantId}/{mappingId}', config: Mapping.getTestMappingData },
     { method: 'GET', path: '/getMappingData/{tenantId}/{mappingId}', config: Mapping.getMappingData },
     { method: 'PUT', path: '/updateMapping/{mappingId}', config: Mapping.updateMapping },
     { method: 'GET', path: '/getMapping/{tenantId}/{mappingId}', config: Mapping.getMappingForEdit },
